c1a19f5f94e81cadddff6579448c7fc62456a96e
Jang Keyte <jangkeyte@gmail.com>
2024-02-29 20:11:47 +0700

    setup 2 plugin acf & um

--- wp-content/themes/newtheme_jangkeyte/functions.php
+++ wp-content/themes/newtheme_jangkeyte/functions.php
@@ -8,7 +8,8 @@
 require get_template_directory() . '/inc/init.php';
 
 /**
- * Lưu ý: Không nên thêm bất kỳ custom code nào ở đây. Vui lòng sử dụng một child theme để các tùy chỉnh của bạn không bị mất trong quá trình cập nhật.
+ * Lưu ý: Không nên thêm bất kỳ custom code nào ở đây. Vui lòng sử dụng một 
+ * child theme để các tùy chỉnh của bạn không bị mất trong quá trình cập nhật.
  * Learn more here: http://codex.wordpress.org/Child_Themes
  */
 
--- wp-content/themes/newtheme_jangkeyte/header.php
+++ wp-content/themes/newtheme_jangkeyte/header.php
@@ -6,33 +6,33 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<title><?php wp_title( '|', true, 'right' ); ?></title>	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    
+    <?php wp_head(); ?>
 
-	<?php do_action('jangkeyte_custom_header_js'); ?>
+    <?php do_action('jangkeyte_custom_header_js'); ?>
 </head>
 
 <body <?php body_class(); // Body classes is added from inc/helpers-frontend.php ?> > 
 
-	<?php do_action('jangkeyte_after_body_open'); ?>
+    <?php do_action('jangkeyte_after_body_open'); ?>
 
-	<div id="wrapper">
+    <div id="wrapper">
 
-		<?php do_action('jangkeyte_before_header'); ?>
+        <?php do_action('jangkeyte_before_header'); ?>
 
-		<header id="header" class="header jangkeyte_header_classes">
-		   	<div class="header-wrapper">
-				<?php get_template_part('template-parts/header/header', 'wrapper');	?>
-		   	</div><!-- header-wrapper-->		   	
-		</header>
+        <header id="header" class="header jangkeyte_header_classes">
+            <div class="header-wrapper">
+                <?php get_template_part('template-parts/header/header', 'wrapper');	?>
+            </div><!-- header-wrapper-->
+        </header>
 
-		<?php do_action('jangkeyte_after_header'); ?>
+        <?php do_action('jangkeyte_after_header'); ?>
 
-		<main id="main" class="jangkeyte_main_classes">
+        <main id="main" class="jangkeyte_main_classes">
